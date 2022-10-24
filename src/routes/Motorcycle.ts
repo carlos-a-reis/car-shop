699902f1fe03a04574877343b848647ec535c052
Carlos Augusto dos Reis <hadqen@gmail.com>
2022-10-24 13:20:35 -0300

    Criação do endpoint /motorcycles/id que permite excluir uma moto do banco de dados baseado em seu id (req 23)

--- src/routes/Motorcycle.ts
+++ src/routes/Motorcycle.ts
@@ -11,7 +11,9 @@ const motorcycleController = new MotorcycleController(motorcycleService);
 
 route.post('/motorcycles', (req, res) => motorcycleController.create(req, res));
 route.get('/motorcycles', (req, res) => motorcycleController.read(req, res));
+// eslint-disable-next-line sonarjs/no-duplicate-string
 route.get('/motorcycles/:id', (req, res) => motorcycleController.readOne(req, res));
 route.put('/motorcycles/:id', (req, res) => motorcycleController.update(req, res));
+route.delete('/motorcycles/:id', (req, res) => motorcycleController.delete(req, res));
 
 export default route;
