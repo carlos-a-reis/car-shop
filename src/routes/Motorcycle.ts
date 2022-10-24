137e7759e425edbf33e49a822d45eb63d81563ba
Carlos Augusto dos Reis <hadqen@gmail.com>
2022-10-24 13:09:16 -0300

    Criação do endpoint /motorcycles que retorna todas as motos do banco de dados (req 20)

--- src/routes/Motorcycle.ts
+++ src/routes/Motorcycle.ts
@@ -10,5 +10,6 @@ const motorcycleService = new MotorcycleService(motorcycle);
 const motorcycleController = new MotorcycleController(motorcycleService);
 
 route.post('/motorcycles', (req, res) => motorcycleController.create(req, res));
+route.get('/motorcycles', (req, res) => motorcycleController.read(req, res));
 
 export default route;
