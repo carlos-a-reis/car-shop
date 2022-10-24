76c0c8195b9d96dfd2a2a8f9e8e3c875b0af652d
Carlos Augusto dos Reis <hadqen@gmail.com>
2022-10-24 13:10:36 -0300

    Criação do endpoint /motorcycles/id que retorna uma moto com base no id (req 21)

--- src/routes/Motorcycle.ts
+++ src/routes/Motorcycle.ts
@@ -11,5 +11,6 @@ const motorcycleController = new MotorcycleController(motorcycleService);
 
 route.post('/motorcycles', (req, res) => motorcycleController.create(req, res));
 route.get('/motorcycles', (req, res) => motorcycleController.read(req, res));
+route.get('/motorcycles/:id', (req, res) => motorcycleController.readOne(req, res));
 
 export default route;
